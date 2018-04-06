52396e00d2f3456fac80412e2a7acce67a7fa877
Madhav Sharma <madhav.sharma@styrolution.com>
2018-04-06 12:55:19 +0200

    Adding details page component and binding the list elements with detail page elements

--- webapp/controller/AppController.controller.js
+++ webapp/controller/AppController.controller.js
@@ -55,8 +55,29 @@ sap.ui.define([
 				sap.ui.core.BusyIndicator.hide();
 				MessageToast.show("Data Loaded!");
 			},
+			//This code will enable the panel container below the list itself and bind the list data accrodingly
+			onItemSelected: function(oEvent){
+				var oSeletedItem = oEvent.getSource();
+				var oContext = oSeletedItem.getBindingContext();
+				//console.log(oContext);
+				var sPath = oContext.getPath();
+				//console.log(sPath);
+				
+				var oDetailsPanel = this.byId("detailsPanel");
+				oDetailsPanel.bindElement({path: sPath});
+				this.byId("detailsPanel").setVisible(true);
+				
+			},
+			//This function is enabling the route to the detail page when list element is selected
 			onNaviToAnother: function(oEvent){
-				this.getRouter().navTo("detailsPage");
+				//var seletedItemId=oEvent.getSource().getBindingContext().getProperty("id");
+				var seletedItemId=oEvent.getSource().getBindingContext().getPath();
+				//console.log("before regEx: " + seletedItemId);
+				seletedItemId = seletedItemId.replace(/[^\d.]/g, "");
+				//console.log("after regEx: " + seletedItemId);
+				this.getRouter().navTo("detailsPage", {
+					photoId: seletedItemId
+				});
 			},
 			// Implementing the Refresh Button
 			refreshDataFromBackend: function(oEvent){
--- webapp/controller/DetailsPage.controller.js
+++ webapp/controller/DetailsPage.controller.js
@@ -4,6 +4,35 @@ sap.ui.define([
 		"use strict";
 		
 		return BaseController.extend("sampleAppUI5Application.controller.DetailsPage", {
+			onInit: function(){
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.getRoute("detailsPage").attachMatched(this._onRouteMatched, this);
+			},
+			//This function binds the data 
+			_onRouteMatched: function(oEvent){
+				var oArgs = oEvent.getParameter("arguments");
+				var sPath = "/photos/" + oArgs.photoId;
+				//console.log(sPath);
+				var oDetailsPanel = this.byId("detailsPanel");
+				oDetailsPanel.bindElement({path: sPath});
+				this.byId("detailsPanel").setVisible(true);
+			},
+			handleEditPress: function(){
+				this._toggleButtonsAndView(true);
+			},
+			handleSavePress: function(){
+				this._toggleButtonsAndView(false);
+			},
+			handleCancelPress: function(){
+				this._toggleButtonsAndView(false);	
+			},
+			//Toggling the View for the Save/Cancel and Edit
+			_toggleButtonsAndView: function(bEdit){
+				var oView = this.getView();
+				oView.byId("edit").setVisible(!bEdit);
+				oView.byId("save").setVisible(bEdit);
+				oView.byId("cancel").setVisible(bEdit);
+			}
 			
 		});
 	});
